0fb5cfe407ec75db1bc8bc0123ff53dabcc1af1d
cityzenKIM <xowns1016@gmail.com>
2024-06-11 10:09:10 +0900

    핫 리로딩 설정

--- shop/src/main.ts
+++ shop/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('listening on port 3000');
+
+  // hot-reload
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
