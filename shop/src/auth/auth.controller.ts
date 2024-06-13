06c1f6a84d156f1601d84f199002adc1e29f7ce6
cityzenKIM <xowns1016@gmail.com>
2024-06-13 18:44:18 +0900

    jwt 로그인 기능 수정

--- shop/src/auth/auth.controller.ts
+++ shop/src/auth/auth.controller.ts
@@ -1,5 +1,8 @@
 import {
+  Body,
   Controller,
+  HttpStatus,
+  Logger,
   Post,
   Req,
   Res,
@@ -9,64 +12,65 @@ import {
 import { AuthService } from './auth.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
-import { LocalAuthGuard } from './local-auth.guard';
 import { JwtAuthGuard } from './jwt/jwt-auth.guard';
+import { LoginRequestDto } from './dto/login-request.dto';
 
 @ApiTags('AUTH')
 @Controller('api/auth')
 export class AuthController {
+  private readonly logger = new Logger('AuthControllerLogger');
   constructor(private readonly authService: AuthService) {}
 
   @ApiOperation({ summary: '로그인' })
-  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Req() req, @Res() res: Response) {
+  async login(@Body() data: LoginRequestDto, @Res() res: Response) {
+    const user = await this.authService.validateUser(data.email, data.password);
     const { accessToken, refreshToken } = await this.authService.generateTokens(
-      req.user,
+      user,
     );
-    res.cookie('refresh_token', refreshToken, { httpOnly: true, secure: true });
-    return {
+
+    res.cookie('refresh_token', refreshToken, {
+      httpOnly: true,
+      secure: true,
+    });
+
+    return res.status(HttpStatus.OK).json({
       statusCode: 200,
       message: '로그인 성공',
       accessToken,
-    };
+    });
   }
 
-  @UseGuards(LocalAuthGuard)
+  @ApiOperation({ summary: '로그아웃' })
+  @UseGuards(JwtAuthGuard)
   @Post('logout')
-  async logout(@Req() req, @Res() res: Response) {
+  async logout(@Res() res: Response) {
     res.clearCookie('refresh_token', {
       httpOnly: true,
       secure: true,
     });
-    return {
+    return res.status(HttpStatus.OK).json({
       statusCode: 200,
-      message: '로그아웃',
-    };
+      message: '로그아웃 성공',
+    });
   }
 
   @ApiOperation({ summary: '토큰 재발급' })
-  @UseGuards(JwtAuthGuard)
   @Post('refresh')
   async refreshToken(@Req() req, @Res() res: Response) {
     const refreshToken = req.cookies['refresh_token'];
+    this.logger.log(refreshToken);
     if (!refreshToken) {
       throw new UnauthorizedException('토큰이 존재하지 않습니다.');
     }
-    try {
-      const newTokens = await this.authService.refreshToken(refreshToken);
-      res.cookie('refresh_token', newTokens.refreshToken, {
-        httpOnly: true,
-        secure: true, // HTTPS 사용 시에만 true
-      });
-      return {
-        statusCode: 200,
-        message: '로그인 성공',
-        accessToken: newTokens.accessToken,
-      };
-    } catch (error) {
-      console.log(error);
-      throw new UnauthorizedException('토큰 생성 실패');
-    }
+    const newTokens = await this.authService.refreshToken(refreshToken);
+    res.cookie('refresh_token', newTokens.refreshToken, {
+      httpOnly: true,
+      secure: true, // HTTPS 사용 시에만 true
+    });
+    return res.status(HttpStatus.OK).json({
+      statusCode: 200,
+      accesToken: newTokens.accessToken,
+    });
   }
 }
--- shop/src/auth/auth.module.ts
+++ shop/src/auth/auth.module.ts
@@ -1,22 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { LocalStrategy } from './local.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    UsersModule,
-    PassportModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      session: false,
+    }),
     JwtModule.register({
       global: true,
       secret: process.env.JWT_ACCESS_SECRET,
       signOptions: { expiresIn: process.env.JWT_ACCESS_EXPIRATION_TIME },
     }),
+    TypeOrmModule.forFeature([Users]),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
