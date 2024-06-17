92773781f71288a3cc782a71b76d0b8242550526
cityzenKIM <xowns1016@gmail.com>
2024-06-17 22:20:19 +0900

    db host 수정

--- shop/src/app.module.ts
+++ shop/src/app.module.ts
@@ -19,7 +19,7 @@ import { UploadsModule } from './uploads/uploads.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       port: 3306,
-      host: process.env.AWS_HOST,
+      host: 'localhost',
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
